cfaaa753ae4ad22feb53eec4a8d81a1d9a585288
Sumit Ahuja <sumitahuja17@gmail.com>
2020-08-11 12:49:40 -0700

    removed jagger

--- src/main/java/com/infosys/api/order/Application.java
+++ src/main/java/com/infosys/api/order/Application.java
@@ -3,7 +3,6 @@ package com.infosys.api.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.jaegertracing.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
