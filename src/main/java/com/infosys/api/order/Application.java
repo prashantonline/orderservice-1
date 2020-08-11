9b23219447bced962137592acb0a6d968dc9e70a
prashantonline <68050143+prashantonline@users.noreply.github.com>
2020-08-11 13:43:53 +0530

    Update Application.java

--- src/main/java/com/infosys/api/order/Application.java
+++ src/main/java/com/infosys/api/order/Application.java
@@ -3,10 +3,55 @@ package com.infosys.api.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Application {
+import io.jaegertracing.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
+@SpringBootApplication
+@ComponentScan({"com.infosys.api.order.*"})
+public class Application extends SpringBootServletInitializer {
+	
+    @Autowired
+    Environment environment;
+    @Value("${spring.application.name}")
+    private String applicationName;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class);
 	}
+	
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            String port = environment.getProperty("local.server.port");
+
+            System.out.println();
+            System.out.println("Server started - http://localhost:" + port + "/health");
+        };
+    }
+	
+  @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    @Bean
+    public io.opentracing.Tracer initTracer() {
+        Configuration.SamplerConfiguration samplerConfig = new Configuration.SamplerConfiguration()
+                .withType("const")
+                .withParam(1);
+        Configuration.ReporterConfiguration reporterConfig = Configuration.ReporterConfiguration.fromEnv()
+                .withLogSpans(true);
+
+        return Configuration.fromEnv(this.applicationName)
+                .withSampler(samplerConfig)
+                .withReporter(reporterConfig).getTracer();
+    }
 }
